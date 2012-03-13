3dd85d90cfd5dd3277648eae129e76fce34d5bff
Joakim Forsberg <joakim.forsberg1@gmail.com>
2012-03-13 22:00:09 +0100

    Init project on github

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webshop::Application.config.secret_token = '2a44a0f5a3c2b59e51fa4b5069114d40a9cf70bfbf666db966bafa5d4a55fdfe121c5b42dab06d265f8560ec664571a00fb2f7f7a3a01ddd5661b6c23dee2aeb'
